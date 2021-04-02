f296af650a903cec1b1cb903f486655dfc3ddcc3
정유찬 <ycjoung0829@gmail.com>
2021-04-02 11:06:50 +0000

    practice scrolling

--- practice/practice.py
+++ practice/practice.py
@@ -1,2 +1,18 @@
-print("hey")
-print("this is my practice file for this project")
+#reference to https://velog.io/@swhybein/python-BeautifulSoup%EC%9C%BC%EB%A1%9C-%ED%81%AC%EB%A1%A4%EB%A7%81%ED%95%98%EA%B8%B0
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://seocho.go.kr/site/seocho/CoronaNewsList.do"
+
+response = requests.get(url)
+
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+#contents > div > div:nth-child(3) > div.sectionContent > p:nth-child(1)
+
+announcement = soup.select('div:nth-child(3) > div.sectionContent > p:nth-child(1)')
+
+print(announcement[0].text)
