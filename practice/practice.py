93aeb956244ea6e21b5499cbe82da55dfe62fbcf
정유찬 <ycjoung0829@gmail.com>
2021-04-03 08:27:16 +0000

    progress

--- practice/practice.py
+++ practice/practice.py
@@ -1,24 +1,23 @@
-"""reference to https://velog.io/@swhybein/python-BeautifulSoup%EC%9C%BC%EB%A1%9C-%ED%81%AC%EB%A1%A4%EB%A7%81%ED%95%98%EA%B8%B0
-"""
 
 import requests
 from bs4 import BeautifulSoup
 
 url = "https://seocho.go.kr/site/seocho/CoronaNewsList.do"
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36"}
 
-response = requests.get(url)
-
+response = requests.get(url, headers = headers)
+response.raise_for_status()
 html = response.text
-
-soup = BeautifulSoup(html, 'html.parser')
-
+soup = BeautifulSoup(html, "lxml")
 
 
-for i in range(3,10):
-    announcement = soup.select('div:nth-child(%d) > div.sectionContent > p:nth-child(1)' %i)
-    txt = announcement[0].text
-    para = txt.split("\n")
-    for sentence in para:
-        print(sentence, "\n")
-    #para = announcement.split("\n")
-    #print(para.find("확진자 경유지 역학조사 결과 미확인 접촉자 발생으로 인하여 확진자의 경유지를 공개합니다"))
+##contents > div > div:nth-child(3) > div.sectionContent > p:nth-child(1)
+ann = soup.select('div:nth-child(3) > div.sectionContent > p:nth-child(1)')
+
+result = ""
+
+for txt in ann:
+    result += txt.text
+start_index = result.find("확진자 경유지 역학조사 결과 미확인 접촉자 발생으로 인하여 확진자의 경유지를 공개합니다")
+end_index = result.find("까지 방문객은 가까운 선별진료소에서 코로나19검사 받으시길 바랍니다.")
+print(result[start_index+48:end_index].strip())
